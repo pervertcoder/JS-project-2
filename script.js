42fbbe4a464462d88b56b3cf91a0e3bb4c830807
Pervertcoder <lance60702@gmail.com>
2025-06-25 19:49:48 +0800

    6/25 afternoon editting

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ document.querySelector(".score").textContent = 55;
 document.querySelector(".guess").value = 17;
 console.log(document.querySelector(".guess").value);
 */
+//challenge 1
+/*
+Implement a game rest functionality, so that the
+player can make a new guess! Here is how:
+1. Select the element with the 'again' class and
+attach a click event handler
+2. In the handler function, restore initial values of
+the score and number variables
+3. Restore the initial conditions of the message,
+number, score and guess input field
+4. Also restore the original background color (#222)
+and number width (15rem)
+*/
 let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector(".check").addEventListener("click", function () {
@@ -49,8 +62,17 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".score").textContent =
       Number(document.querySelector(".score").textContent) + 1;
     */
-    document.querySelector("body").style.background = "#60b347";
+    document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = number;
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".score").textContent = "20";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Start Guessing ...";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".guess").value = "";
 });
