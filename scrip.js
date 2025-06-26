7425849b3b0747e5c9ef35588f10117f61d0670e
Pervertcoder <lance60702@gmail.com>
2025-06-26 19:36:59 +0800

    6/26 afternoon editting

--- scrip.js
+++ scrip.js
@@ -3,6 +3,10 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
-const btnOpenModal = document.querySelector(".show-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
 
-console.log(btnOpenModal);
+//console.log(btnOpenModal);
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i].textContent);
+}
