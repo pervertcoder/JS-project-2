f2c39fcf6717891c1d42b0865a4e4f9f0f4129ca
Pervertcoder <lance60702@gmail.com>
2025-06-27 08:43:17 +0800

    6/27 morning editting

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-'use strict';
+"use strict";
+
+const score1EL = document.querySelector("#score--0");
+const score2EL = document.getElementById("score--1");
+const diceEL = document.querySelector(".dice");
+
+score1EL.textContent = 0;
+score2EL.textContent = 0;
+diceEL.classList.add("hidden");
