a3f4eecae5a2639820d67c2c5ddf0e143a5f9632
Pervertcoder <lance60702@gmail.com>
2025-06-29 11:48:15 +0800

    6/28 #pigGameProject

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 //selecting elements
+const player0EL = document.querySelector(".player--0");
+const player1EL = document.querySelector(".player--1");
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.getElementById("score--1");
 const currentScore0EL = document.getElementById("current--0");
@@ -11,7 +13,19 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+const score = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0EL.classList.toggle("player--active");
+  player1EL.classList.toggle("player--active");
+};
+
 /*
 const testFunction = function () {
   console.log("btn clicked");
@@ -25,18 +39,48 @@ diceEL.classList.add("hidden");
 //rolling dice functionality
 
 btnRoll.addEventListener("click", function () {
-  //1. generating a random dice roll
-  let dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    //1. generating a random dice roll
+    let dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  //2.display the dice
-  diceEL.classList.remove("hidden");
-  diceEL.src = `dice-${dice}.png`;
+    //2.display the dice
+    diceEL.classList.remove("hidden");
+    diceEL.src = `dice-${dice}.png`;
 
-  //3.check for roll 1
-  if (dice !== 1) {
-    currentScore += dice;
-    currentScore0EL.textContent = currentScore;
+    //3.check for roll 1
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //switch player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  //1.Add the current score to active player's score
+  score[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    score[activePlayer];
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+  //2.check if the score >= 100
+  if (score[activePlayer] >= 20) {
+    //finish the game
+    playing = false;
+    diceEL.classList.add("hidden");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
   } else {
+    //switch to the next player
+    switchPlayer();
   }
 });
